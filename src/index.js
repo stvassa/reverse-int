5232519ef42c5ecba3c4cdd397ae9bf2adced42f
Vassa <vassatret@gmail.com>
2022-07-23 13:03:05 +0300

    feat: solution reverse int

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+  let result = '';
+  n = Math.abs(n);
+  n = String(n);
+  for (let i = 0; i < n.length; i += 1) {
+    result = `${n[i]}${result}`
+  };
+  result = Number(result);
+  return result;
 }
